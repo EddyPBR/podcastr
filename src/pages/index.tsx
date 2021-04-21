80cf76aa5e4456b963be211f7fbf44e1aae09c52
EddyPBR <edvaldojunior1310@gmail.com>
2021-04-21 17:10:13 -0300

    :package: Setting SSG | Consuming server-json api

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,40 @@
-export default function Home() {
+interface IEpisodes {
+  id: string;
+  title: string;
+  members: string;
+  published_at: string;
+  thumbnail: string;
+  description: string;
+  file: {
+    url: string;
+    type: string;
+    duration: number;
+  };
+}
+
+interface IHomeProps {
+  episodes: IEpisodes[];
+}
+
+export default function Home(props: IHomeProps) {
   return (
-    <h1>Home page</h1>
-  )
+    <div>
+      <h1>Index</h1>
+      <p>{JSON.stringify(props.episodes)}</p>
+    </div>
+  );
+}
+
+export async function getStaticProps() {
+  const response = await fetch("http://localhost:3333/episodes");
+  const data: IEpisodes[] = await response.json();
+
+  const EIGHT_HOURS = 28800;
+
+  return {
+    props: {
+      episodes: data,
+    },
+    revalidate: EIGHT_HOURS,
+  };
 }
