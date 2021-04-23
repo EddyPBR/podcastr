ce47e7a499c832139a8f6c84fa9b0011bd234d63
EddyPBR <edvaldojunior1310@gmail.com>
2021-04-23 10:37:24 -0300

    :package: Adding play function to episode page

--- src/pages/episodes/[slug].tsx
+++ src/pages/episodes/[slug].tsx
@@ -1,10 +1,16 @@
 import { GetStaticProps, GetStaticPaths } from "next";
 import Image from "next/image";
 import Link from "next/link";
+
+import { usePlayer } from "../../contexts/PlayerContext";
+
 import { format, parseISO } from "date-fns";
 import ptBR from "date-fns/locale/pt-BR";
+
 import { api } from "../../services/api";
+
 import { convertDurationToTimeString } from "../../utils/convertDurationToTimeString";
+
 import styles from "./episode.module.scss";
 
 type Episode = {
@@ -24,6 +30,8 @@ type EpisodeProps = {
 };
 
 export default function Episode({ episode }: EpisodeProps) {
+  const { play } = usePlayer();
+
   return (
     <div className={styles.episode}>
       <div className={styles.thumbnailContainer}>
@@ -38,7 +46,7 @@ export default function Episode({ episode }: EpisodeProps) {
           src={episode.thumbnail}
           objectFit="cover"
         />
-        <button>
+        <button type="button" onClick={() => play(episode)}>
           <img src="/play.svg" alt="Tocar episódio" />
         </button>
       </div>
